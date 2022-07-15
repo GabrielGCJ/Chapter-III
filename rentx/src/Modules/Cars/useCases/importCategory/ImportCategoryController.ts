b0cf8a34a293756b0eeb4c8fbd5f606da6126936
Gabriel Jorge <gabriel.ggcj@gmail.com>
2022-07-15 03:47:51 -0300

    Correção de bug specifications

--- rentx/src/Modules/Cars/useCases/importCategory/ImportCategoryController.ts
+++ rentx/src/Modules/Cars/useCases/importCategory/ImportCategoryController.ts
@@ -4,12 +4,12 @@ import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
 class ImportCategoryController {    
 
-    handle(request: Request, response: Response): Response {
-        const { file } = request
+    async handle(request: Request, response: Response): Promise <Response> {
+        const { file } = request 
 
         const importCategoryUseCase = container.resolve(ImportCategoryUseCase) 
 
-        importCategoryUseCase.execute(file) 
+        await importCategoryUseCase.execute(file as any) 
         
         return response.send() 
     }
