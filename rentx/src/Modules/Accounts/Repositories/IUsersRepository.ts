fde44ba2fe0ce01b4f782ce37fdf6d21fd7c5bc1
Gabriel Jorge <gabriel.ggcj@gmail.com>
2022-07-18 15:22:25 -0300

    Adição de verificação de email existente

--- rentx/src/Modules/Accounts/Repositories/IUsersRepository.ts
+++ rentx/src/Modules/Accounts/Repositories/IUsersRepository.ts
@@ -1,9 +1,10 @@
 import { ICreateUserDTO } from "../DTOs/ICreateUserDTO"
+import { User } from "../Entities/User"
 
 
 interface IUsersRepository {
     create(data:ICreateUserDTO ):Promise<void>
-
+    findByEmail(email:string):Promise <User>
 }
 
 export { IUsersRepository }
