cb1eb2800a626943794fbd5172287f4346909c3e
Gabriel Jorge <gabriel.ggcj@gmail.com>
2022-07-18 13:22:01 -0300

    Criação de controller de usuario

--- rentx/src/Modules/Accounts/useCases/createUser/CreateUserUseCase.ts
+++ rentx/src/Modules/Accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,9 +1,9 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../../DTOs/ICreateUserDTO";
 import { IUsersRepository } from "../../Repositories/IUsersRepository";
 
 
-
+@injectable()
 class CreateUserUseCase {
 
 
@@ -11,8 +11,6 @@ class CreateUserUseCase {
         @inject("UsersRepository")
         private usersRepository: IUsersRepository
     ) {}
-
-
 
 
     async execute({ name, username, email, password, driver_license }:ICreateUserDTO):Promise<void> {
